05b3e55bbace4600e9d68e0f9cd9e1e54eadb393
ReginaSilvaC <regina.amy.sc@gmail.com>
2025-04-25 20:34:18 -0300

    Terceira aula IA na EDN

---
+++ Exercicios/estudo.py
@@ -0,0 +1,28 @@
+# sequencialidade - acordar
+print("Acordei")
+print("Ir ao banheiro")
+print("Tomar café")
+print("Escovar os dentes")
+
+# Definição clara
+print("Vamos somar 2 numeros me dia qual e digite")
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+soma = numero1 + numero2
+print("A soma é: ", soma)
+
+# finutude
+for i in range(5):
+    print("Contar", i)
+    print("Não tem fim") 
+
+    for i in range(5):
+    print("Contar", i)
+    if i == 2:
+        break
+    print("Não tem fim")
+
+# determinismo
+def soma(a, b):
+    return a + b 
+print(soma(4, 5))
