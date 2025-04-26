9e89cbda0a26d14a67f03931b31adffbf5450520
ReginaSilvaC <regina.amy.sc@gmail.com>
2025-04-25 22:04:03 -0300

    Terceira aula de IA na EDN

---
+++ Exercicios/Algoritmos.py
@@ -0,0 +1,28 @@
+# Algoritmo de busca 
+'''def busca(lista, nome_procurado):
+    for i in range(len(lista)):
+        if lista[i] == nome_procurado:
+            return i + 1 # retorna para a posisão
+    return -1 # não encontrou
+nomes = ["Maria", "Ana", "João", "Pedro"]
+posicao = busca(nomes, "Ana")
+nome_procurado = input("Qual nome você quer procurar? ")
+print("Ana está na posição:", posicao)'''
+
+# Algoritimo de busca binária
+def binaria(lista, valor_procurado):
+    inicio = 0
+    fim = len(lista) - 1
+    while inicio <= fim:
+        meio = (inicio + fim) // 2
+        if lista[meio] == valor_procurado:
+            return meio
+        elif lista[meio] < valor_procurado:
+            inicio = meio + 1
+        else:
+            fim = meio - 1
+    return -1
+numeros = [1, 3, 5, 7, 9, 11, 13, 15] 
+posicao = binaria(numeros, 15) 
+print("Número encontrato na pocição:", posicao)         
+
